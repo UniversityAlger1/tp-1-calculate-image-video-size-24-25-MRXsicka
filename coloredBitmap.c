f738e4736238947dbdab87680e1159d1bc48df18
MRXsicka <89555799+MRXsicka@users.noreply.github.com>
2024-11-22 22:25:05 +0100

    Update coloredBitmap.c

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -8,7 +8,22 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
+ // gray scale image size in bits
+    int grayScale= w * h * 8;
 
+    // Colored image  size in bits (24 bits per pixel for a colored image)
+    float color = grayScale * 3;
+
+    // Convert based on the unit
+    if (strcmp(unit, "bt") == 0) {
+        return color; // Bits
+    } else if (strcmp(unit, "ko") == 0) {
+        return color / 1024; // Kilobits
+    } else if (strcmp(unit, "mo") == 0) {
+        return color / (1024 * 1024); // Megabits
+    } else if (strcmp(unit, "go") == 0) {
+        return color / (1024 * 1024 * 1024); // Gigabits
+    }
    // YOUR CODE HERE - END
    return 0;
 }
