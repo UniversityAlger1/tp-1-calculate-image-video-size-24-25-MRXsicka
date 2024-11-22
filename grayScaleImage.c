aa5837eddffc128b90e6dca03c08a592fc6400db
MRXsicka <89555799+MRXsicka@users.noreply.github.com>
2024-11-22 22:10:16 +0100

    Update grayScaleImage.c

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -6,7 +6,8 @@
 //   grayScale size of the image Bitmap (in byte)
 float grayScaleImage(int w, int h) {
    // YOUR CODE HERE - BEGIN
-
+int size=w*h;//because grayscale images use 1 byte per pixel and the size should be in bytes
+   printf("The size of the image is : %d",&size);
    // YOUR CODE HERE - END
    return 0;
 }
